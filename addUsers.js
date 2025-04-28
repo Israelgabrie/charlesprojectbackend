6d5deaf428d108e0172c9d3a0f2c57b005d6447c
Israelgabrie <101274418+Israelgabrie@users.noreply.github.com>
2025-04-28 13:23:45 +0100

    28-april-2025-2nd-commit

--- addUsers.js
+++ addUsers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const { User, RecentActivity } = require("./database");
 const dotenv = require("dotenv").config();
 
